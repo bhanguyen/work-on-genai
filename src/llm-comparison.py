ef873939fa485f63cd24cfe4ff7860131888685d
bhanguyen <17997701+bhanguyen@users.noreply.github.com>
2024-07-26 21:51:36 +1000

    refactored chat models

--- src/llm-comparison.py
+++ src/llm-comparison.py
@@ -8,23 +8,26 @@ import streamlit as st
 import os
 import PyPDF2
 from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
 
 # Import langchain libraries for prompting and chat models
 from langchain_community.chat_models import ChatOllama
 from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableSequence
 from langchain.callbacks import StreamlitCallbackHandler
-import anthropic
 
 # Import LlamaIndex libraries for document processing and retrieval
 from llama_index.core import Document
 
-# Import custom vectorstore module
+# Import custom module
 from applications.qa_bot.modules.vectorstore import (
     get_llama_index_retriever, 
     process_documents
 )
+from vector_visual import main
+
 # Load environment variables
 load_dotenv()
 
@@ -57,86 +60,42 @@ def get_prompt_template(context: str, question: str):
         "Context: {context}\n\nQuestion: {question}\n\nAnswer:"
     )
 
-def get_openai_response(context: str, question: str, model: str, temperature):
-    # Initialize OpenAI model
-    openai_llm = ChatOpenAI(model=model, openai_api_key=openai_api_key, temperature=temperature)
+def get_llm_response(context: str, question: str, model: str, temperature:float):
+    # Define prompt template
     prompt_template = get_prompt_template(context, question)
-    openai_chain = RunnableSequence(prompt_template | openai_llm)
-    openai_response = openai_chain.invoke(
-        {"context": context, "question": user_prompt}, 
+
+    # Innitialize the appropriate chat model based on the selected model name
+    if model in model_categories["Ollama"]:
+        llm = ChatOllama(model=model, temperature=temperature)
+    elif model in model_categories["Anthropic"]:
+        llm = ChatAnthropic(model=model, anthropic_api_key=anthropic_api_key, temperature=temperature)
+    elif model in model_categories["OpenAI"]:
+        llm = ChatOpenAI(model=model, openai_api_key=openai_api_key, temperature=temperature)
+    else:
+        raise ValueError(f"Unknown model: {model}")
+    
+    # Set up the chain
+    chain = RunnableSequence(prompt_template | llm)
+    response = chain.invoke(
+        {"context": context, "question": user_prompt},
         config={"callbacks": [StreamlitCallbackHandler(st.container())]}
     )
-    return openai_response.content
-
-# Function to get Claude 3.5 Sonnet response
-def get_claude_response(context: str, question: str, model: str, temperature):
-    anthropic_client = anthropic.Anthropic(api_key=anthropic_api_key)
-    message = anthropic_client.messages.create(
-        model=model,
-        max_tokens=1000,
-        temperature=temperature,
-        system="You are a helpful AI assistant. Use the provided context to answer the user's question.",
-        messages=[
-            {"role": "user", "content": f"Context: {context}\n\nQuestion: {question}"}
-        ]
-    )
-    return message.content[0].text
-
-# Function to get Ollama's response
-def get_ollama_response(context: str, question: str, model: str, temperature):
-    # Initialize Ollama LLM with the selected model
-    ollama_llm = ChatOllama(model=model, temperature=temperature)
-    prompt_template = get_prompt_template(context, question)
-    ollama_chain = RunnableSequence(prompt_template | ollama_llm)
-    ollama_response = ollama_chain.invoke(
-        {"context": context, "question": user_prompt}, 
-        config={"callbacks": [StreamlitCallbackHandler(st.container())]}
-    )
-    return ollama_response.content
+    return response.content
 
 # Display response
-def display_response(model, prompt):
-    model_categories = {
-        "Ollama": ["phi3", "llama3", "mistral", "gemma2"],
-        "Anthropic": ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-2.1"],
-        "OpenAI": ["gpt-4", "gpt-3.5-turbo"]
-    }
+def display_response(context, question, model, temperature):
     try:
-        if model in model_categories["Ollama"]:
-            with st.spinner("Generating..."):
-                    ollama_response = get_ollama_response(
-                        context=context, 
-                        question=user_prompt,
-                        model=model, 
-                        temperature=temperature
-                    )
-            st.write(ollama_response)
-            
-        elif model in model_categories["Anthropic"]:
-            with st.spinner("Generating..."):
-                    claude_response = get_claude_response(context, user_prompt)
-            st.write(claude_response)
-
-        elif model in model_categories["OpenAI"]:
-            with st.spinner("Generating..."):
-                    openai_response = get_openai_response(
-                        context=context,
-                        question=user_prompt,
-                        model=model,
-                        temperature=temperature
-                    )
-            st.write(openai_response)
-        else:
-            raise ValueError(f"Unknown model: {model}")
+        with st.spinner("Generating..."):
+            response = get_llm_response(context=context, question=question, model=model, temperature=temperature)
+            st.write(response)
     except Exception as e:
         st.error(f"Error generating response from {model}: {str(e)}")
     
-# Streamlit UI setup
-st.set_page_config(layout="wide")
+# Streamlit UI Main App
 st.title("LLM Response Comparison with LlamaIndex :llama:")
-st.write("This application compare the responses between different LLM including phi3, llama3, mistral, openai, anthropic")
+st.write("This application compare the responses between different LLMs hosted with OpenAI, Anthropic, and Ollama")
 
-# Setup sidebar
+# Uploading documents process
 with st.sidebar:
     # Upload documents
     st.subheader("Your documents")
@@ -155,14 +114,13 @@ with st.sidebar:
             vector_store = process_documents(documents)
             st.success('PDF uploaded successfully!', icon="✅")
 
-# Ollama model selection in the sidebar
-# ollama_models = ["phi3", "llama3", "mistral"]
-# selected_ollama_model = st.sidebar.selectbox("Select Ollama Model", ollama_models)
+# Define model categories and models
 model_categories = {
 "Ollama": ["phi3", "llama3", "mistral", "gemma2"],
 "Anthropic": ["claude-3-haiku-20240307", "claude-3-sonnet-20240229", "claude-3-5-sonnet-20240620"],
-"OpenAI": ["gpt-40-mini", "gpt-3.5-turbo"]
+"OpenAI": ["gpt-4o-mini", "gpt-3.5-turbo"]
 }
+
 with st.sidebar:
     # Initialize session state for selections
     if 'selected_models' not in st.session_state:
@@ -186,30 +144,41 @@ selected_models = [model for models in st.session_state.selected_models.values()
 # Sidebar information and warnings
 st.sidebar.title("Settings")
 # Set temperature 
-temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=2.0, value=0.0, step=0.01)
+# temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=2.0, value=0.0, step=0.01)
+temperature = 0
 st.sidebar.info("Make sure to set your API keys and connection string as environment variables:\n\n"
                 "OPENAI_API_KEY\n"
                 "ANTHROPIC_API_KEY")
 st.sidebar.warning("Ensure Ollama is running locally on the default port.")
 
+# Constants
+HOST = os.environ.get("DB_HOST")
+USER = os.environ.get("DB_USER")
+PASSWORD = os.environ.get("DB_PASSWORD")
+PORT = os.environ.get("DB_PORT")
+DATABASE = os.environ.get("PGVECTOR_DATABASE")
+
 # Main input area for user prompt
 user_prompt = st.text_input("Enter your prompt:", placeholder="Your query")
-from vector_visual import main
+
 with st.expander("Check Query Embedding"):
-    context_node_ids = [
-                'e6cc36a4-4ac1-4840-b3e7-3360fb7b68b3',
-                '18d79297-540a-41b5-90f5-79ae1dcb196e',
-                '093d9c81-4db6-4f41-a7bb-439130ecb013',
-                '9d493f75-7b05-4ce0-9b2c-fe123414fc20',
-                'd756cc58-a2fb-45b1-9f64-a85fbda72599',
-                '0187ecc2-238e-49d6-8695-f943253c7c8f',
-                '7e0f7f7c-86b0-43c6-a839-2764528c2e8a',
-                '18a1329d-6f35-468e-bb8f-cbc69d89187f',
-                'd5b18386-dc02-4885-84f3-5c94ea52b086',
-                '59c41c65-77e0-46a1-99e3-b8cfad25efe7',
-                'e8c1d19f-0200-4129-b875-207af0e4ac9e',
-                '1f2a47e5-7a42-4a3d-9c33-ab50a3201456'
-            ]
+
+    # Database connection details
+    CONNECTION_STRING = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+
+    # Create an engine
+    engine = create_engine(CONNECTION_STRING)
+
+    # Define the query
+    query = "SELECT node_id FROM data_llamaindex;"
+
+    # Connect to the database and execute the query
+    with engine.connect() as connection:
+        result = connection.execute(text(query))
+        context_node_ids = [row[0] for row in result.fetchall()]
+
+    # Print the populated list
+    print(context_node_ids)
     main(context_node_ids, user_prompt)
 
 if st.button("Generate Responses", key="response"):
@@ -229,52 +198,17 @@ if st.button("Generate Responses", key="response"):
                 st.write(doc.score)
         
         with tab3:
-    #         # Create three columns for displaying LLM responses
-    #         col1, col2, col3 = st.columns(3)
-            
-    #         # Generate and display Ollama response
-    #         with col1:
-    #             st.subheader(f"Ollama ({selected_ollama_model})")
-    #             with st.spinner("Generating..."):
-    #                 ollama_response = get_ollama_response(
-    #                     context=context, 
-    #                     question=user_prompt,
-    #                     model=selected_ollama_model, 
-    #                     temperature=temperature
-    #                 )
-    #             st.write(ollama_response)
-            
-    #         # Generate and display Anthropic (Claude 3.5 Sonnet) response
-    #         with col2:
-    #             st.subheader("Anthropic (Claude 3.5 Sonnet)")
-    #             with st.spinner("Generating..."):
-    #                 claude_response = get_claude_response(context, user_prompt)
-    #             st.write(claude_response)
-            
-    #         # Generate and display OpenAI response
-    #         with col3:
-    #             st.subheader("OpenAI (GPT-3.5)")
-    #             with st.spinner("Generating..."):
-    #                 openai_response = get_openai_response(
-    #                     context=context,
-    #                     question=user_prompt,
-    #                     model="gpt-3.5-turbo",
-    #                     temperature=temperature
-    #                 )
-    #             st.write(openai_response)
-    # else:
-    #     st.warning("Please enter a question.")
-            
-
             tabs = st.tabs(selected_models)
             for i, tab in enumerate(tabs):
                 with tab:
-                    display_response(selected_models[i], user_prompt)
+                    display_response(context, user_prompt, selected_models[i], temperature)
+    else:
+        st.warning("Please enter a question.")
 
 # Check for missing API keys and connection string
 if not openai_api_key:
     st.sidebar.error("OpenAI API key is not set. Please set the OPENAI_API_KEY environment variable.")
 if not anthropic_api_key:
     st.sidebar.error("Anthropic API key is not set. Please set the ANTHROPIC_API_KEY environment variable.")
-# if not CONNECTION_STRING:
-#     st.sidebar.error("PostgreSQL connection string is not set. Please set the PG_CONNECTION_STRING environment variable.")
+if not CONNECTION_STRING:
+    st.sidebar.error("PostgreSQL connection string is not set. Please set the PG_CONNECTION_STRING environment variable.")
--- src/vector_visual.py
+++ src/vector_visual.py
@@ -98,6 +98,13 @@ def main(context_node_ids, question):
     # Input query
     query = question
 
+    # Get the embedding for the query
+    query_embedding = get_query_embedding(query)
+
+    # Display the actual vector values
+    st.subheader("Query Embedding (Original)")
+    st.write(str(query_embedding))
+
     # Example node_ids for context
     context_node_ids = context_node_ids
 
@@ -108,16 +115,9 @@ def main(context_node_ids, question):
         st.error("No context embeddings found in the database.")
         return
 
-    # Get the embedding for the query
-    query_embedding = get_query_embedding(query)
-
     if query_embedding.shape[0] != context_embeddings.shape[1]:
         st.error(f"The dimension of the query embedding {query_embedding.shape[0]} does not match the context embeddings {context_embeddings.shape[1]}.")
         return
-
-    # Display the actual vector values
-    st.subheader("Query Embedding (Original)")
-    st.write(str(query_embedding))
 
     # Option to select visualization type
     visualization_type = st.selectbox("Select visualization type", ("2D", "3D"))
@@ -143,19 +143,5 @@ def main(context_node_ids, question):
 
 if __name__ == "__main__":
 
-    context_node_ids = [
-        'e6cc36a4-4ac1-4840-b3e7-3360fb7b68b3',
-        '18d79297-540a-41b5-90f5-79ae1dcb196e',
-        '093d9c81-4db6-4f41-a7bb-439130ecb013',
-        '9d493f75-7b05-4ce0-9b2c-fe123414fc20',
-        'd756cc58-a2fb-45b1-9f64-a85fbda72599',
-        '0187ecc2-238e-49d6-8695-f943253c7c8f',
-        '7e0f7f7c-86b0-43c6-a839-2764528c2e8a',
-        '18a1329d-6f35-468e-bb8f-cbc69d89187f',
-        'd5b18386-dc02-4885-84f3-5c94ea52b086',
-        '59c41c65-77e0-46a1-99e3-b8cfad25efe7',
-        'e8c1d19f-0200-4129-b875-207af0e4ac9e',
-        '1f2a47e5-7a42-4a3d-9c33-ab50a3201456'
-    ]
     query = st.text_input("Enter your query:")
     main(context_node_ids, query)
